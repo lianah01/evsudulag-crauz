7827c071fb0e82087c1a959c1ef6db86ba786c05
lianah01 <julianacrauz@gmail.com>
2025-03-11 09:15:14 +0800

    activity4

---
+++ my-lumen-api/app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = DB::table('users')->where('username', $request->username)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        if (!$user || $request->password !== $user->password) {
+            return response()->json([
+                'message' => 'Invalid username or password'
+            ], 401);
+        }
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+                'gender' => $user->gender,
+                'address' => $user->address,
+                'occupation' => $user->occupation
+            ]
+        ]);
+    }
+}
